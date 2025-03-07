e38f2035b74e292cb242cb55d4a26a15bff39d19
강동욱 <ehddnr8563@gmail.com>
2025-03-07 15:49:39 +0900

    feat: Tabs 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css';
 import MainLayout from './components/common/MainLayout';
+import Tabs, {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from './components/common/Tabs';
 
 function App() {
   return (
     <>
-      <MainLayout>test</MainLayout>
+      <MainLayout>
+        <Tabs>
+          <TabList>
+            <Tab index={0}>tab1</Tab>
+            <Tab index={1}>tab2</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel index={0}>tab1 contents</TabPanel>
+            <TabPanel index={1}>tab2 contents</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </MainLayout>
     </>
   );
 }
